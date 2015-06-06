e3c5d8a06f1a4a72ee9da347bad5b131623101ea
Joe <joe.guilmette@gmail.com>
2015-06-06 06:56:21 -0400

    fuck

--- custom/aliases.bash
+++ custom/aliases.bash
@@ -1,3 +1,4 @@
 alias plz='sudo "$BASH" -c "$(history -p !!)"'
 alias lockdown='sudo /etc/lockdown/lockdown.sh'
 alias openitup='sudo usermod -s /bin/bash www-data'
+alias fuck='eval $(thefuck $(fc -ln -1)); history -r'
