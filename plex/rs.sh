6ef9732b08b452dea9bcd64f8d540420b5c004cb
Joe Guilmette <joe.guilmette@gmail.com>
2018-05-22 11:13:40 +0700

    wrong port

--- plex/rs.sh
+++ plex/rs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rs() {
-	rsync -avz --stats --partial --progress -e 'ssh -p 41055' mynameisjoe@mynameisjoe.cloud.seedboxes.cc:~/files/Plex/*/*"$1"*/* ~/Movies/
+	rsync -avz --stats --partial --progress -e 'ssh -p 41035' mynameisjoe@mynameisjoe.cloud.seedboxes.cc:~/files/Plex/*/*"$1"*/* ~/Movies/
 }
